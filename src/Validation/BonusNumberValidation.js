6ef9eb1d5183a3f85c75868b1abc9442990482ff
DOBOB_LAPTOP <dlehgus1110@naver.com>
2022-11-29 15:55:38 +0900

    feat: 보너스 번호 중복 검증 기능 구현

--- src/Validation/BonusNumberValidation.js
+++ src/Validation/BonusNumberValidation.js
@@ -2,14 +2,22 @@ const { ERROR_MSG } = require('../constants/message.js');
 const { LOTTO } = require('../constants/condition.js');
 
 class BonusNumberValidation {
-  static validateBonusNumber(bonusNumber) {
+  static validateBonusNumber(bonusNumber, winningNumber) {
     if (!this.#isValidBonusNumberRange(bonusNumber)) {
       throw new Error(ERROR_MSG.invalidLottoNumberRange);
+    }
+
+    if (!this.#isUniqueBonusNumber(bonusNumber, winningNumber)) {
+      throw new Error(ERROR_MSG.duplicatedLottoNumber);
     }
   }
 
   static #isValidBonusNumberRange(input) {
     return LOTTO.numberMinRange <= Number(input) && Number(input) <= LOTTO.numberMaxRange;
+  }
+
+  static #isUniqueBonusNumber(bonusNumber, winningNumber) {
+    return !winningNumber.split(',').includes(bonusNumber);
   }
 }
 
